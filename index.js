949c7dda882cc6016b29f346cc6559ff6ab534b7
Shubham <princeshubham22699@gmail.com>
2018-09-22 13:22:01 +0530

    Mongoose Part-2

--- index.js
+++ index.js
@@ -4,33 +4,56 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 
 const url = "mongodb://localhost:27017/conFusion";
-const connect = mongoose.connect(url , {
-    useMongoClient : true
+const connect = mongoose.connect(url, {
+    useMongoClient: true
 });
 
 connect.then((db) => {
 
-    var newDish = Dishes({
+    //  *** Method-2 Using .Create() Method of Dishes
+    Dishes.create({
         name: "Uthapizza",
         description: "text"
-    });
+    })
+    .then((dish) => {
+        console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return db.collection("dishes").drop();
-        })
-        .then(() => {
-            return db.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        return db.collection("dishes").drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 });
 
+/*  ***  Method-1 First method for Creation of Dish Using new Keyword  ***
+var newDish = Dishes({
+    name: "Uthapizza",
+    description: "text"
+});
+
+newDish.save()
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return db.collection("dishes").drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+*/
